90029b2d32b7902c5b79a086776f5b04e0cd9256
Panther1986 <polzovatel77@gmail.com>
2024-05-22 12:52:43 +0300

    update1

---
+++ src/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import { env } from './utils/env.js';
+
+const PORT = Number(env('PORT', '3002'));
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(express());
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
