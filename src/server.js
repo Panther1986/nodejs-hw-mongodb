06e8d8b89af1e644307bbe2def5f3d9351310120
Panther1986 <polzovatel77@gmail.com>
2024-05-22 16:05:50 +0300

    update2

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utils/env.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
 
 const PORT = Number(env('PORT', '3002'));
 
@@ -18,6 +19,31 @@ export const setupServer = () => {
       },
     }),
   );
+
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello',
+    });
+  });
+
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+
+    res.status(200).json({
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  });
+
+  app.get('/contacts/:contactId', async (req, res) => {
+    const { contactId } = req.params();
+    const student = await getContactById(contactId);
+
+    res.status(200).json({
+      message: `Successfully found contact with id ${contactId}!`,
+      data: student,
+    });
+  });
 
   app.use('*', (req, res, next) => {
     res.status(404).json({
