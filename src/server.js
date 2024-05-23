f5c83fca036a7ed713199eaa3c95b22d83c12635
Panther1986 <polzovatel77@gmail.com>
2024-05-23 10:32:45 +0300

    update4

--- src/server.js
+++ src/server.js
@@ -30,6 +30,7 @@ export const setupServer = () => {
     const contacts = await getAllContacts();
 
     res.status(200).json({
+      status: 200,
       message: 'Successfully found contacts!',
       data: contacts,
     });
@@ -40,6 +41,7 @@ export const setupServer = () => {
     const student = await getContactById(contactId);
 
     res.status(200).json({
+      status: 200,
       message: `Successfully found contact with id ${contactId}!`,
       data: student,
     });
