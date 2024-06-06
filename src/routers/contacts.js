978c19441fbf319f4877b97584db2f23243bf09a
Panther1986 <polzovatel77@gmail.com>
2024-06-06 23:32:33 +0300

    update8

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -7,6 +7,11 @@ import {
   patchContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import {
+  createContactsShema,
+  updateContactsShema,
+} from '../validation/contacts.js';
 
 const router = Router();
 
@@ -14,9 +19,17 @@ router.get('/contacts', ctrlWrapper(getContactsController));
 
 router.get('/contacts/:contactId', ctrlWrapper(getContactsByIdController));
 
-router.post('/contacts', ctrlWrapper(createContactController));
+router.post(
+  '/contacts',
+  validateBody(createContactsShema),
+  ctrlWrapper(createContactController),
+);
 
-router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
+router.patch(
+  '/contacts/:contactId',
+  validateBody(updateContactsShema),
+  ctrlWrapper(patchContactController),
+);
 
 router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
 
