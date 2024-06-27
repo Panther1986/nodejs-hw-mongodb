0a6d98d95c98bb152624708ace55fa34ed60e11a
Panther1986 <polzovatel77@gmail.com>
2024-06-27 12:12:44 +0300

    update28

--- src/constans/index.js
+++ src/constans/index.js
@@ -1,4 +1,4 @@
-import path from 'node:path';
+import path from 'path';
 
 export const SORT_ORDER = {
   ASC: 'asc',
@@ -13,6 +13,8 @@ export const SMTP = {
   SMTP_USER: 'SMTP_USER',
   SMTP_PASSWORD: 'SMTP_PASSWORD',
   SMTP_FROM: 'SMTP_FROM',
+  JWT_SECRET: 'JWT_SECRET',
+  APP_DOMAIN: 'APP_DOMAIN',
 };
 
-export const TEMPLATES_DIR = path.join(process.cwd(), 'scr', 'templates');
+export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
